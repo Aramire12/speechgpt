0067fb21835b3aa9ad424d5f8b51d47efd1bf79d
Aramire12 <136098533+Aramire12@users.noreply.github.com>
2023-06-09 19:25:38 +0200

    Update BrowserSpeechToText.tsx

--- src/components/BrowserSpeechToText.tsx
+++ src/components/BrowserSpeechToText.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import {} from './Notification';
-import { handleSend } from './Content';
 
 interface BrowserSpeechToTextProps {
   isListening: boolean;
